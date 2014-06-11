db6125e859c12d223a3155a6da19563f537557c3
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-06-11 18:08:25 +0700

    delete html file

--- src/js/services/config_service.js
+++
@@ -1,8 +0,0 @@
-
-app.factory("configService", function(){
-    var endPoint = "http://10.0.0.77:8877";
-
-    return {
-     endPoint : endPoint
-    };
-});
--- src/js/services/folters.js
+++
@@ -1,9 +0,0 @@
-
-app.filter('range', function() {
-    return function(input, total) {
-        total = parseInt(total);
-        for (var i=0; i<total; i++)
-            input.push(i);
-        return input;
-    };
-});
